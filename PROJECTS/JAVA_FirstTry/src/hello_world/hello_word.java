ef1e342f08a55cd763f7ea58aa8e41f7810e1253
Maxime Richaudeau <maxime.richaudeau1@gmail.com>
2021-09-15 10:15:08 +0200

    Test changes on new branch

--- PROJECTS/JAVA_FirstTry/src/hello_world/hello_word.java
+++ PROJECTS/JAVA_FirstTry/src/hello_world/hello_word.java
@@ -2,6 +2,6 @@ package hello_world;
 
 public class hello_word {
     public static void main(String[] args) {
-    System.out.println("Hello World !!");
+    System.out.println("Hello les TSE !!");
     }
 }
