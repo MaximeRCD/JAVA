d732fcdea0b68977038ceeecdb998e6408af82eb
Maxime Richaudeau <maxime.richaudeau1@gmail.com>
2021-09-15 10:29:26 +0200

    Test class method

--- PROJECTS/JAVA_FirstTry/src/hello_world/hello_word.java
+++ PROJECTS/JAVA_FirstTry/src/hello_world/hello_word.java
@@ -3,6 +3,6 @@ package hello_world;
 public class hello_word {
     public static void main(String[] args) {
         String message = " Tu te mets à JAVA ?";
-        System.out.println(message);
+        System.out.println(message.toUpperCase());
     }
 }
