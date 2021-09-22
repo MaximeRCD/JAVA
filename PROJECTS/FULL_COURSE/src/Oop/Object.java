e0787778d1afd9452193dabb082c0bd61c8b9b6e
Maxime Richaudeau <maxime.richaudeau1@gmail.com>
2021-09-22 11:18:35 +0200

    Test first class object

---
+++ PROJECTS/FULL_COURSE/src/Oop/Object.java
@@ -0,0 +1,17 @@
+package Oop;
+
+public class Object {
+    private String a_property;
+
+    public Object(String my_property){
+        a_property = my_property;
+    }
+
+    public String getA_property() {
+        return a_property;
+    }
+
+    public void setA_property(String a_property) {
+        this.a_property = a_property;
+    }
+}
---
+++ PROJECTS/FULL_COURSE/src/Oop/main.java
@@ -0,0 +1,9 @@
+package Oop;
+
+public class main {
+    public static void main(String[] args) {
+        Object first_object = new Object("This is an object");
+        first_object.setA_property("Ohh in fact that's a book");
+        System.out.println(first_object.getA_property());
+    }
+}
