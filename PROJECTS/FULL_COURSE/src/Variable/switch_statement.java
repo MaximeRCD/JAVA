b21f14dcab2dc04be7b92b2703259682a0c07601
Maxime Richaudeau <maxime.richaudeau1@gmail.com>
2021-09-23 09:05:40 +0200

    Switch statement

---
+++ PROJECTS/FULL_COURSE/src/Variable/switch_statement.java
@@ -0,0 +1,14 @@
+package Variable;
+
+public class switch_statement {
+    public static void main(String[] args) {
+        String day = "Friday";
+        switch(day){
+            case "Friday":
+                System.out.println("It's week end");
+                break;
+            default:
+                System.out.println("Not yet weekend");
+        }
+    }
+}
